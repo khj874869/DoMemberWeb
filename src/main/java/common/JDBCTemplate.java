247769253f6383c9816d91741e88f2292e50d1eb
khj874869 <134674147+khj874869@users.noreply.github.com>
2023-07-28 19:54:15 +0900

    Update JDBCTemplate.java

--- src/main/java/common/JDBCTemplate.java
+++ src/main/java/common/JDBCTemplate.java
@@ -3,64 +3,73 @@ package common;
 import java.sql.*;
 
 public class JDBCTemplate {
-	
+
+	// 싱글톤 패턴 적용
 	private static JDBCTemplate instance;
 	private static Connection conn;
-	private  JDBCTemplate () {		
-	}
+	
+	private JDBCTemplate() {}
+	
 	public static JDBCTemplate getInstance() {
-		if(instance==null) {
+		if(instance == null) {
 			instance = new JDBCTemplate();
-			
 		}
 		return instance;
 	}
+	
 	public Connection createConnection() {
-		String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
-		String url = "jdbc:oracle:thin:@localhost:1521:XE";
-		String ID ="MEMBERWEB";
-		String PW = "MEMBERWEB";
-				
+		String driverName = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "MEMBERWEB";
+		String password = "MEMBERWEB";
 		try {
-			if(conn ==null || conn.isClosed()) {
-				Class.forName(DRIVER_NAME);
-				conn = DriverManager.getConnection(url,ID,PW);
-				conn.setAutoCommit(false);
+			if(conn == null || conn.isClosed()) {
+				Class.forName(driverName);
+				conn = DriverManager.getConnection(url, user, password);
+				conn.setAutoCommit(false); // 오토커밋 해제
 			}
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
-		}
-				
-		return conn;
-	}
-	public void close(Connection conn) {
-		if(conn!=null) {
-			try {
-				if(conn.isClosed()) conn.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-	public void commit(Connection conn) {
-		if(conn !=null) {
-			try {
-			if(conn.isClosed())	conn.commit();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-	public void rollback(Connection conn) {
-		if(conn!=null) {
-		try {
-        if(conn.isClosed())			conn.rollback();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}}
+		return conn;
+	}
+	/*
+	 * 연결해제
+	 */
+	public void close(Connection conn) {
+		if(conn != null) {
+			try {
+				if(!conn.isClosed()) conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	/*
+	 * 커밋하기
+	 */
+	public void commit(Connection conn) {
+		if(conn != null) {
+			try {
+				if(!conn.isClosed()) conn.commit();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	/*
+	 * 롤백하기
+	 */
+	public void rollback(Connection conn) {
+		if(conn != null) {
+			try {
+				if(!conn.isClosed()) conn.rollback();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
