916054af2ef1125eec5ffdeeeb320a82dddd6cbe
khj874869 <134674147+khj874869@users.noreply.github.com>
2023-07-28 20:00:41 +0900

    Update memberUpdate.java

--- src/main/java/member/controller/memberUpdate.java
+++ src/main/java/member/controller/memberUpdate.java
@@ -37,27 +37,28 @@ public class memberUpdate extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		String memberId = request.getParameter("id");
-		String memberPw = request.getParameter("pw");
-		String memberGender = request.getParameter("gender");
-		String memberEmail=request.getParameter("email");
-		String memberPhone=request.getParameter("phone");
-		String memberAddress=request.getParameter("addr");
-		String memberHobby=request.getParameter("hobby");
-		//UPDATE MEMBER_WEB SET MEMBER_PW = ?,MEMBER_PHONE=?,MEMBER_ADDRESS=?,MEMBER_HOBBY=? WHERE MEMBER_ID = ?, UPDATE_DATE =SYSDATE;
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");	// 한글아 깨지지마~!
+		String memberId = request.getParameter("member-id");
+		String memberPw = request.getParameter("member-pw");
+		String memberGender = request.getParameter("member-gender");
+		String memberEmail = request.getParameter("member-email");
+		String memberPhone = request.getParameter("member-phone");
+		String memberAddress = request.getParameter("member-address");
+		String memberHobby = request.getParameter("member-hobby");
+		// UPDATE MEMBER_TBL SET MEMBER_PW = ?, MEMBER_EMAIL = ?, MEMBER_PHONE = ?, MEMBER_ADDRESS = ?, MEMBER_HOBBY = ?, UPDATE_DATE = SYSDATE WHERE MEMBER_ID = ?
 		MemberService service = new MemberService();
-		Member member=new Member(memberId, memberPw, memberGender, memberEmail, memberPhone, memberAddress, memberHobby);
 //		Member member = new Member(memberId, memberPw, memberEmail, memberPhone, memberAddress, memberHobby);
+		Member member = new Member(memberId, memberPw, memberGender, memberEmail, memberPhone, memberAddress, memberHobby);
+//		int result = service.updateMember(memberId, memberPw, memberEmail, memberPhone, memberAddress, memberHobby);
 		int result = service.updateMember(member);
-		if(result>0) {
-			//성공메인페이지
-			response.sendRedirect(".index.jsp");
-		}
-		else {
-			request.setAttribute("msg", "회원정보 수정이 완료되지 못 했습니다.");
-			request.getRequestDispatcher("/member/serviceFail.jsp").forward(request,response);
-			//실패
+		if(result > 0) {
+			// 성공하면 메인페이지
+			response.sendRedirect("/index.jsp");
+		}else {
+			// 실패하면 에러페이지
+			request.setAttribute("msg", "회원 수정이 완료되지 않았습니다.");
+			request.getRequestDispatcher("/member/serviceFailed.jsp").forward(request, response);
 		}
 	}
 
