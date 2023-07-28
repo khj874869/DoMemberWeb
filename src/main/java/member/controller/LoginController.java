587158218375ab0e02def13aad26cfec0f86108a
khj874869 <134674147+khj874869@users.noreply.github.com>
2023-07-28 17:45:50 +0900

    [ADD] 23/07/28 회원 로그인,로그아웃 기능 추가 회원 로그인, 로그아웃 컨트롤러 추가

---
+++ src/main/java/member/controller/LoginController.java
@@ -0,0 +1,68 @@
+package member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import member.model.service.MemberService;
+import member.model.vo.Member;
+
+/**
+ * Servlet implementation class loginController
+ */
+@WebServlet("/member/Login.do")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String memberId = request.getParameter("id");
+		String memberPw = request.getParameter("pw");
+		Member member = new Member(memberId,memberPw);
+	    //select * from MEMBER_WEB  where MEMBER_ID = ?and MEMBER_PW = ?
+		MemberService service= new MemberService();
+		Member mOne = service.selectOneById(member);
+		HttpSession session = request.getSession();
+		session.setAttribute("memberId", member.getMemberId());
+		session.setAttribute("memberName", member.getMemberName());
+		if(mOne!=null) {
+			request.setAttribute("msg","전송성공했습니다.");
+			request.setAttribute("URL", "/index.jsp");
+			RequestDispatcher view = request.getRequestDispatcher("/member/ServiceSuccess.jsp");
+			view.forward(request, response);
+
+		}else {
+			request.setAttribute("msg","전송실패했습니다.");
+			RequestDispatcher view = request.getRequestDispatcher("/member/ServiceFail.jsp");
+			view.forward(request, response);			
+			}
+
+		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+
+}
---
+++ src/main/java/member/controller/LogoutController.java
@@ -0,0 +1,47 @@
+package member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutController
+ */
+@WebServlet("/member/Logout.do")
+public class LogoutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		if(session !=null) {
+			session.invalidate(); //세션 저장정보 사라짐
+			response.sendRedirect("/index.jsp");
+			//자바에서의 location.href 
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
